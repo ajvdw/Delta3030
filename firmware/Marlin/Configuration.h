523f1da53a1ed875be7d1f4d944fe68f228b8c74
ajvdw <ajvandewerken@gmail.com>
2018-04-28 15:27:10 +0200

    Default bed temperatures

--- firmware/Marlin/Configuration.h
+++ firmware/Marlin/Configuration.h
@@ -1272,12 +1272,12 @@
 // @section temperature
 
 // Preheat Constants
-#define PREHEAT_1_TEMP_HOTEND 180
-#define PREHEAT_1_TEMP_BED     70
+#define PREHEAT_1_TEMP_HOTEND 190
+#define PREHEAT_1_TEMP_BED     40
 #define PREHEAT_1_FAN_SPEED   255 // Value from 0 to 255
 
 #define PREHEAT_2_TEMP_HOTEND 240
-#define PREHEAT_2_TEMP_BED    100
+#define PREHEAT_2_TEMP_BED    60
 #define PREHEAT_2_FAN_SPEED   255 // Value from 0 to 255
 
 /**
